48b336c9c73593dcaadcb73c57f856abfb509a85
DzmitryPresnakou <7745834@gmail.com>
2024-09-09 12:28:34 +0300

    first commit

---
+++ common/src/test/java/com/dmdev/integration/ExampleIT.java
@@ -0,0 +1,13 @@
+package com.dmdev.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleIT {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
